55abf1db4a4806dd18d8402120949caaadd57fdf
matearg <mateoarcidiacono@gmail.com>
2022-05-05 23:36:08 -0300

    Added exercices

---
+++ 5-unidad-Arreglos/trabajo-4/ejercicio7-terminar/main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define CANT_E 100
+
+// Realizar una función que inserte un carácter en un
+// arreglo ordenado alfabéticamente,
+// conservando el orden.
+
+int cargarArreglo(char a[]);
+void mostrarArrelgo(char a[], int validos);
+void ordenarArrelgo(char a[], int validos);
+
+int main()
+{
+    int validos = 0;
+    char arreglo[CANT_E];
+
+    validos = cargarArreglo(arreglo);
+
+    printf("\nArrelgo luego de carga\n\n");
+    mostrarArrelgo(arreglo, validos);
+
+    printf("\n\n");
+    system("PAUSE");
+
+    return 0;
+}
+
+int cargarArreglo(char a[])
+{
+    char condicion = 's';
+    int validos = 0;
+
+    do
+    {
+        printf("Ingrese un caracter al arreglo ");
+        fflush(stdin);
+        scanf("%c", &a[validos]);
+
+        validos++;
+
+        printf("Desea continuar s/n ");
+        fflush(stdin);
+        scanf("%c", &condicion);
+    }
+    while (condicion == 's');
+
+    return validos;
+}
+
+void mostrarArrelgo(char a[], int validos)
+{
+    int i = 0;
+
+    for(i=0;i<validos;i++)
+    {
+        printf("| %c ", a[i]);
+    }
+}
+
+void ordenarArrelgo(char a[], int validos)
+{
+}
