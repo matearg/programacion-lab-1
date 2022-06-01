4b95a1248525f74559364fdee0563c4a18122488
matearg <mateoarcidiacono@gmail.com>
2022-06-01 13:20:50 -0300

    Ejercicio 1 trabajo 5 agregado

---
+++ 5-unidad-Arreglos/trabajo-5/ejercicio1/main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+#include <string.h>
+
+// Crear una función que los cargue, hasta que el usuario lo decida
+
+int cargarArreglos(int l[], char n[][30], int a[]);
+void mostrarArreglos(int l[], char n[][30], int a[], int validos);
+
+int main()
+{
+    int legajos[20];
+    char nombres[20][30];
+    int anios[20];
+    int validos = 0;
+
+    validos = cargarArreglos(legajos, nombres, anios);
+
+    printf("\n\nLista de alumnos:\n");
+    mostrarArreglos(legajos, nombres, anios, validos);
+
+    return 0;
+}
+
+int cargarArreglos(int l[], char n[][30], int a[])
+{
+    srand(time(NULL));
+
+    int i = 0, numLegajo = 1;
+    char condicion;
+
+    do
+    {
+        l[i] = numLegajo;
+        numLegajo++;
+
+        printf("\nIngrese el nombre del alumno: ");
+        fflush(stdin);
+        scanf("%s", n[i]);
+
+        a[i] = rand()%5 + 1;
+
+        printf("\nDesea agregar otro alumno s/n: ");
+        fflush(stdin);
+        condicion = getch();
+
+        i++;
+    }
+    while (condicion == 's' && i < 20);
+
+    return i;
+}
+
+void mostrarArreglos(int l[], char n[][30], int a[], int validos)
+{
+    int i = 0;
+
+    printf("Legajos\t Nombre\t Anios\t");
+
+    for(i = 0; i < validos; i++)
+    {
+        printf("\n%d\t", l[i]);
+        printf("%s\t", n[i]);
+        printf("%d", a[i]);
+    }
+}
